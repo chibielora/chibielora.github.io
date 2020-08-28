831b931376f1f048b9524e4d6ba3751d91fe2809
chibielora <chibi.elora@gmail.com>
2020-08-27 23:52:11 -0400

    added spotify links, fixed github calendar, added work history, added mailto

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,11 +63,11 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "IonicaBizau");
+    GitHubCalendar("#github-graph", "chibielora", { responsive: true, tooltips: true });
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "mdo", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "chibielora", selector: "#ghfeed" });
 
 
 });
