55f344855bbcd00ca4d851f556ecb5e314061ec2
chibielora <chibi.elora@gmail.com>
2020-08-28 12:50:43 -0400

    hid rss feed

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,39 +28,39 @@ jQuery(document).ready(function($) {
     
     /* jQuery RSS - https://github.com/sdepold/jquery-rss */
     
-    $("#rss-feeds").rss(
+    // $("#rss-feeds").rss(
     
-        //Change this to your own rss feeds
-        "https://feeds.feedburner.com/TechCrunch/startups",
+    //     //Change this to your own rss feeds
+    //     "https://feeds.feedburner.com/TechCrunch/startups",
         
-        {
-        // how many entries do you want?
-        // default: 4
-        // valid values: any integer
-        limit: 3,
+    //     {
+    //     // how many entries do you want?
+    //     // default: 4
+    //     // valid values: any integer
+    //     limit: 3,
         
-        // the effect, which is used to let the entries appear
-        // default: 'show'
-        // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
-        effect: 'slideFastSynced',
+    //     // the effect, which is used to let the entries appear
+    //     // default: 'show'
+    //     // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
+    //     effect: 'slideFastSynced',
         
-        // will request the API via https
-	    // default: false
-	    // valid values: false, true
-	    ssl: true,
+    //     // will request the API via https
+	//     // default: false
+	//     // valid values: false, true
+	//     ssl: true,
         
-        // outer template for the html transformation
-        // default: "<ul>{entries}</ul>"
-        // valid values: any string
-        layoutTemplate: "<div class='items'>{entries}</div>",
+    //     // outer template for the html transformation
+    //     // default: "<ul>{entries}</ul>"
+    //     // valid values: any string
+    //     layoutTemplate: "<div class='items'>{entries}</div>",
         
-        // inner template for each entry
-        // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
-        // valid values: any string
-        entryTemplate: '<div class="item"><h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fas fa-external-link-alt"></i>Read more</a></div></div>'
+    //     // inner template for each entry
+    //     // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
+    //     // valid values: any string
+    //     entryTemplate: '<div class="item"><h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fas fa-external-link-alt"></i>Read more</a></div></div>'
         
-        }
-    );
+    //     }
+    // );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
     GitHubCalendar("#github-graph", "chibielora", { responsive: true, tooltips: true });
